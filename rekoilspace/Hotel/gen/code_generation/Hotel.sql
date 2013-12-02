40740b7f2bdc6bc7949e99710cb1db99377e8e49
Adrian Bjugård <adrian@qng.se>
2013-12-02 13:58:16 +0100

    initial commit

---
+++ rekoilspace/Hotel/gen/code_generation/Hotel.sql
@@ -0,0 +1,7 @@
+-- root-types-contained: SystemModel_c
+-- BP 7.1 content: StreamData syschar: 3 persistence-version: 7.1.6
+
+INSERT INTO S_SYS
+	VALUES ("c37961c9-8899-46bd-93b8-3bc84b36128b",
+	'Hotel',
+	0);
---
+++ rekoilspace/Hotel/gen/code_generation/_system.sql
@@ -0,0 +1,7 @@
+-- root-types-contained: SystemModel_c
+-- BP 7.1 content: StreamData syschar: 3 persistence-version: 7.1.6
+
+INSERT INTO S_SYS
+	VALUES (1,
+	'Hotel',
+	0);
